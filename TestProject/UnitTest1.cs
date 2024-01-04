8a6884a5f2104623b9b781d18f6b4db61fdaa2d7
MargaritaJarovaja <Nord.ferro@gmail.com>
2024-01-04 20:54:55 +0100

    Smoke test add

---
+++ TestProject/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    [TestClass]
+    public class SmokeTest
+    {
+        private readonly HttpClient client;
+
+        public SmokeTest()
+        {
+            this.client = new HttpClient();
+        }
+
+        [TestMethod]
+        public async Task HomePage_Returns200OK()
+        {
+           
+            var response = await client.GetAsync("https://blazorforazure.azurewebsites.net/");
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
